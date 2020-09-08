c4ab284516d674082f5bc4c07a1da278f062ac02
NightRoadIx <quintanarxbio@gmail.com>
2020-09-08 01:13:24 -0500

    Add files via upload

---
+++ recuperame/recuperame/MainForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: NightRoadIx
+ * Date: 07/09/2020
+ * Time: 16:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace recuperame
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(203, 28);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Forma de pago:";
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 40);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(203, 21);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.ClientSize = new System.Drawing.Size(557, 233);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Datos de Amazon";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ recuperame/recuperame/MainForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using System.Net;	// Paquetería para manejar archivos desde una URL
+using System.Linq;	// Paquetería con datos adicionales para el manejo de listas 
+
+namespace recuperame
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		// PROPIEDADES DE LA CLASE MAINFORM
+		// Donde se guardan los encabezados
+		List<string> hutt = new List<string>();
+		// Lista con los valores únicos de pago
+		List<string> pagos = new List<string>();
+		// IEnumerable<string> pagos = new List<string>();
+		// Lista para guardar todos los datos
+		List<string[]> data = new List<string[]>();		
+		
+		
+		// METODOS DE LA CLASE MAINFORM 
+		// Método para obtener los datos de un CSV a partir de una URL
+		public static string obtenerCSV(string url)
+		{
+			// Se crea el pedido de una URL
+			HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+			// Aquí es donde se manejará la respuesta que entregue esta URL
+			HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+			
+			// TODO: Hay que manejar las excepciones que ocurran, sobre todo cuando no exista el archivo o se presente un error de lectura
+			// TODO: Generar una rutina para mostrar al usuario que se esta leyendo el archivo
+			
+			// Depués se crea un "streaming" para la lectura "en directo" del archivo solicitado
+			StreamReader sr = new StreamReader(resp.GetResponseStream());
+			// Se leerá el archivo hasta el final para asignarlo a una cadena de texto
+			string results = sr.ReadToEnd();
+			// IMPORTANTE: Cerrar el streaming de lectura
+			sr.Close();
+			
+			return results;
+		}		
+		
+		// Método principal de la ventana
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		// Cuando la ventana se genera o carga
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+			// Variable temporal
+			List<string> tmp = new List<string>();
+			
+			// Dirección del recurso CSV a tratar
+			string destinoURL = "http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv";
+			
+			// Llamar a la función para obtener un archivo CSV directamente de una URL
+			string archon = obtenerCSV(destinoURL);
+			
+			/* Leer el contenido del archivo línea por línea */
+			// Generar una cadena de texto temporal
+			string aLine;
+			// Un contador temporal
+			int j = 0;
+			// Crear un objeto StringReader, pasando como argumento al constructor el texto
+			StringReader strReader = new StringReader(archon);
+
+			// Recorrer todo el archivo
+			while(true) 
+			{
+				// Leer línea a línea de la cadena de texto
+				aLine = strReader.ReadLine();
+				
+				// Mientras no se halle el caracter null, realizar la lectura
+				if(aLine != null)
+				{
+					// cuando se encuentre la primera línea
+					if(j == 0)
+					{
+						// Recorrer elemento a elemento de la cadena
+						// separados por comas
+						foreach(string en in aLine.Split(','))
+							hutt.Add(en);
+					}
+					// En otro caso, cuando no sea el encabezado
+					// se almacenan todos los datos de la tabla
+					else
+					{
+						data.Add(aLine.Split(','));
+						// Aquí se puede añadir los valores del tipo de pago
+						tmp.Add(aLine.Split(',')[3]);
+					}
+				}
+				else // EOF = End Of File  ---> texto como null
+					break;
+				// Incrementar el contador
+				j++;
+			}			
+			
+			// IMPORTANTE: Cerrar el Lector de Cadenas
+			strReader.Close();
+			
+			// Generar la lista de pagos con datos únicos (sin repetir)
+			pagos = tmp.Distinct().ToList();
+			
+			// Rellenar el Combobox con los valores obtenidos
+			foreach(string a in pagos)
+				comboBox1.Items.Add(a);
+			// Colocar el primer valor como defecto
+			comboBox1.Text = comboBox1.Items[0].ToString();
+		}
+	}
+}
